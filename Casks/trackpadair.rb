f53433fd43bbc3a95f72cad1deea99086d1d5b7d
NakaokaRei <reideeplearning@gmail.com>
2023-02-23 23:11:07 +0900

    Add TrackpadAir v0.1.0

---
+++ Casks/trackpadair.rb
@@ -0,0 +1,11 @@
+cask "trackpadair" do
+  version "0.1.0"
+  sha256 "021a927ec3e8d48e7511b0f2b3e1716cd5cdb7a93546670cef0fffbc179f076c"
+
+  url "https://github.com/NakaokaRei/TrackpadAir/releases/download/#{version}/TrackpadAir.zip"
+  name "TrackadAir"
+  desc "TrackpadAir is macOS app that allows you to operate without a trackpad"
+  homepage "https://github.com/NakaokaRei/TrackpadAir"
+
+  app "TrackpadAir.app"
+end
